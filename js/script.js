52e3b22f68c158e6af3d5f29b56fa528436562c8
Gowri Bhat <gowri.bhat@u.nus.edu>
2023-10-21 18:47:26 +0800

    Change to one test chart with many stages

--- js/script.js
+++ js/script.js
@@ -1,42 +1,31 @@
 $(document).ready(function() {
   // Define your data for Gantt chart 1 (e.g., Bread Counter)
-  var breadCounterData = [
-      {
-          name: "Order 1",
-          desc: "Ordering and Preparation",
-          values: [{
-              from: "/Date(1634822400000)/",
-              to: "/Date(1634826000000)/",
-              label: "Ordering and Preparation"
-          }]
-      },
-      // Add more tasks for the Bread Counter
+  var ganttData = [
+    {
+        name: "Bread Counter",
+        desc: "Ordering and Preparation",
+        values: [{
+            from: "/Date(1634822400000)/",
+            to: "/Date(1634826000000)/",
+            label: "Ordering and Preparation"
+        }]
+    },
+    {
+        name: "Toaster Counter",
+        desc: "Toasting",
+        values: [{
+            from: "/Date(1634822400000)/",
+            to: "/Date(1634823000000)/",
+            label: "Toasting"
+        }]
+    },
+    // Add more stages or counters for food preparation
   ];
-
-  // Define your data for Gantt chart 2 (e.g., Toaster Counter)
-  var toasterCounterData = [
-      {
-          name: "Order 1",
-          desc: "Toasting",
-          values: [{
-              from: "/Date(1634822400000)/",
-              to: "/Date(1634823000000)/",
-              label: "Toasting"
-          }]
-      },
-      // Add more tasks for the Toaster Counter
-  ];
+  
 
   // Initialize Gantt chart for the Bread Counter
   $("#chart1").gantt({
-      source: breadCounterData,
-      scale: "hour", // Customize the time scale if needed
-      // Configure additional options and styles
-  });
-
-  // Initialize Gantt chart for the Toaster Counter
-  $("#chart2").gantt({
-      source: toasterCounterData,
+      source: ganttData,
       scale: "hour", // Customize the time scale if needed
       // Configure additional options and styles
   });
